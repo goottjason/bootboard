154fc4fc398736875ad78f792eb59ce211dec3ce
goottjason <goottjason@gmail.com>
2025-04-30 17:58:00 +0900

    resultMap의 detailPage webconfig까지 끝

---
+++ src/main/java/com/miniproj/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.miniproj.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Value("${file.upload-base-dir}")
+  private String uploadDir;
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/upload/**")
+      .addResourceLocations("file:" + uploadDir);
+  }
+}
